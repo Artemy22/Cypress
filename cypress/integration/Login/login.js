ab25c038431903a8408e3b70cd59592f5ddba7bd
Artemy22 <artem.nii@me.com>
2022-05-30 15:30:17 +0300

    Dfx login (#6)
    * the Login page test done
    
    * go to the step definitions implemented

--- cypress/integration/Login/login.js
+++ cypress/integration/Login/login.js
@@ -3,31 +3,28 @@ import ProfilePage from '../../support/PageObjects/ProfilePage'
 
 describe('Automation Test for the Login Page', function () {
 
-
-    after(function(){
+    after(function () {
         const profilePage = new ProfilePage;
         profilePage.clickDropDownUser().clickButtonSubit();
     })
 
     const loginPage = new LoginPage;
 
-
-Given('I navigate to the login page', ()=> {
-    cy.visit(Cypress.env('base_url'))
-    cy.contains('Sign in to DFX')
-})
-
+    Given('I navigate to the login page', () => {
+        cy.visit(Cypress.env('base_url'))
+        cy.contains('Sign in to DFX')
+    })
 
     When('I enter credentials', () => {
         loginPage.fillEmail();
         loginPage.fillPassword();
     });
 
+    When('Click on the Login bnt', () => {
+        loginPage.clickSubmitButton();
+    });
 
-When('Click on the Login bnt', ()=> {
-    loginPage.clickSubmitButton();
+    Then('My user is logged in', () => {
+        cy.contains('Your profile')
+    })
 });
-
-Then('My user is logged in', ()=> {
-    cy.contains('Your profile')
-})});
