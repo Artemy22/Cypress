b960197869deef61dc991e1dcdefa99b189aac40
Artemy22 <artem.nii@me.com>
2022-06-01 12:37:43 +0300

    ProfilePAgeUpdated

---
+++ cypress/support/PageObjects/EditProfilePopup.js
@@ -0,0 +1,58 @@
+
+class EditProfilePopup {
+
+    inputFirstName = '#firstName'
+    inputLastname = '#lastName'
+    inputJobTitle = '#job_title'
+    inputOrganisation = '#org_name'
+    inputEmailAddress = '#email'
+    inputPhoneNumber = '#phone'
+    inputLinkedInURL = '#linkedin_url'
+    inputLocation = '#location'
+    buttonSave = '#submit'
+
+    fillInputFirstName() {
+        cy.get(this.inputFirstName).click();
+        return this;
+    }
+
+    fillInputLastname() {
+        cy.get(this.inputFirstName).click();
+        return this;
+    }
+
+    fillInputFirstName() {
+        cy.get(this.inputFirstName).click();
+        return this;
+    }
+    
+    fillInputFirstName() {
+        cy.get(this.inputFirstName).click();
+        return this;
+    }
+    
+    fillInputFirstName() {
+        cy.get(this.inputFirstName).click();
+        return this;
+    }
+    
+    fillInputFirstName() {
+        cy.get(this.inputFirstName).click();
+        return this;
+    }
+    
+    fillInputFirstName() {
+        cy.get(this.inputFirstName).click();
+        return this;
+    }
+    
+    fillInputFirstName() {
+        cy.get(this.inputFirstName).click();
+        return this;
+    }
+
+
+
+
+
+}
--- cypress/support/PageObjects/ProfilePage.js
+++ cypress/support/PageObjects/ProfilePage.js
@@ -1,13 +1,20 @@
 class ProfilePage {
     dropDownUser = '.css-b4y3aj';
     buttonSignOut = '[href="/sign-out"]';
+    buttonEditProfile = '.css-59wuf6';
+
+
 
     clickDropDownUser(){
-        cy.get(this.dropDownUser).click()
+        cy.get(this.dropDownUser).click();
         return this;
     }
     clickButtonSubit(){
-        cy.get(this.buttonSignOut).click()
+        cy.get(this.buttonSignOut).click();
+        return this;
+    }
+    clickButtonEditProfile() {
+        cy.get(this.buttonEditProfile).click();
         return this;
     }
 }
